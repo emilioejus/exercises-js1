6440d2bb118f7e1340bd47ed08d0cd77f24373be
emilioejus <emilioejus@gmail.com>
2021-02-18 15:29:09 +0100

    agregando la tarea en el homeWork de la semana-1 en F-strings-methods

---
+++ week-1/Homework/F-strings-methods/exercise.js
@@ -0,0 +1,23 @@
+// Start by creating a variable `message`
+
+let message = "Hello world";
+
+console.log(message);
+
+// Utilizando la propiedad .length 
+let luis = "Luis";
+let cLuis = luis.length;
+
+console.log(`My name is ${luis} and my name is ${cLuis} characters longs!`);
+
+
+
+let nombre = "Emilio";
+console.log(`Mi bombre es ${nombre} y mi nombre tiene ${nombre.length} caracteres de largo!!!`);
+
+// Utilizando la propiedad .toUpperCase y .toLowerCase
+let emilio = "Emilio";
+let ramon = "Ramon";
+
+console.log(`Hola soy ${emilio.toUpperCase()} y mi nombre esta en mayúscula`);
+console.log(`Hola soy ${ramon.toLowerCase()} y mi nombre esta en minúscula`);
---
+++ week-1/Homework/F-strings-methods/exercise2.js
@@ -0,0 +1,14 @@
+//Utilizando .trim()
+
+const names = " Daniel  ";
+let message = `         Hola soy ${names}         `
+
+console.log(message);
+console.log(message.trim());
+
+
+let str = "       Hello World!        ";
+
+console.log(str);
+console.log(str.trim());
+
